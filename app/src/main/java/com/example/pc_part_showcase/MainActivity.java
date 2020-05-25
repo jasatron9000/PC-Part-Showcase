b2fe05949675979a16ca63c72c1ff76fc166322d
Jason Buquiran <jbuq319@UoA.auckland.ac.nz>
2020-05-25 14:23:44 +1200

    Initial commit

---
+++ app/src/main/java/com/example/pc_part_showcase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pc_part_showcase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
